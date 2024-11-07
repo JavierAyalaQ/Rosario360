453dc6fdaa896a2fce1710da837ec0ef6b9ca580
Javier Ayala <javierayala1618@gmail.com>
2024-11-07 08:10:45 -0500

    commit

--- src/components/cards/PlaceCardR.jsx
+++ src/components/cards/PlaceCardR.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const PlaceCard = ({ key, name, description, image, href }) => {
     return (
         <article
+            key={key}
             className="max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         >
             <a href={href}>
@@ -24,16 +25,6 @@ const PlaceCard = ({ key, name, description, image, href }) => {
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-4 text-ellipsis">
                     {description}
                 </p>
-                {/* 
-                <a
-                    href={href}
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-600 rounded-lg transition-colors duration-200 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                    data-astro-prefetch=""
-                >
-                    Ver más
-                    <i className="fa-solid fa-arrow-right ml-2 text-sm"></i>
-                </a>
-                 */}
                 <a
                     href={href}
                     className="text-sm font-medium text-gray-600 transition-all hover:text-gray-900 dark:text-neutral-400 dark:hover:text-white
